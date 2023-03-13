9e1b11eebb9f8299a2c1de812e6fae50bf0f35f3
Mark Koenig <markkoenig03@gmail.com>
2023-03-12 21:36:44 -0400

    adding fixes for mobile friendly
    Major changes to naming conventions and HTML

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const secBtns = document.querySelectorAll('.controls');
-const secBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function PageTransitions() {
-    // Button click active class
-    for (let i = 0; i < secBtn.length; i++) {
-        secBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
-    }
-
-    //Sections active class e= event
-    allSections.addEventListener('click', (e) => {
-        const id = e.target.dataset.id;
-        if (id) {
-            //remove selected from the other btns 
-            secBtns.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-            //hide other sections 
-            sections.forEach((section) => {
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
-}
-
-PageTransitions();
+})();
